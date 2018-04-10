8987ffbfd6a6ca59952dfd89287b5cacfecf8c1c
Jaap Weijland <jaapweijland@gmail.com>
2018-04-10 16:14:43 +0200

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const algoliasearch = require('algoliasearch');
+const bodyParser = require('body-parser');
 
 const client = algoliasearch('8HYBSNX4Q5', '5d225d11ef765b21fb13bc97688801ef');
 
@@ -16,6 +17,13 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/search', (req, res) => {
     if(!req.query.term){
         return res.status(400).send('Please provide req.query.term as a String');
